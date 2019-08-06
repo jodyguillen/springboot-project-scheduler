aa6d58a840e9ee6104ded3d5b9f99d7b0ea2735f
sparkblitz <devsparkblitz@gmail.com>
2019-08-06 09:53:46 +0800

    bare minimum (upload, persist, view)

---
+++ src/main/java/io/sparkblitz/sps/SpringbootProjectSchedulerApplication.java
@@ -0,0 +1,13 @@
+package io.sparkblitz.sps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectSchedulerApplication.class, args);
+	}
+
+}
