c45bb3c82b169eff0853355572c305e3c78ba1c3
sparkblitz <devsparkblitz@gmail.com>
2019-08-06 11:28:02 +0800

    calendaring

---
+++ src/main/java/io/sparkblitz/sps/services/SchedulingService.java
@@ -0,0 +1,27 @@
+package io.sparkblitz.sps.services;
+
+import io.sparkblitz.sps.domain.Activity;
+import io.sparkblitz.sps.domain.Project;
+import io.sparkblitz.sps.scheduling.Calendar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class SchedulingService {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ActivityService activityService;
+
+    public Calendar plotCalendar(Long projectId, Date startDate) {
+        Project project = projectService.getById(projectId);
+        Calendar calendar = new Calendar(project, startDate);
+        calendar.schedule((activityCode) -> activityService.listNext(activityCode));
+        return calendar;
+    }
+}
